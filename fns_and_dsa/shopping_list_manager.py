f440cb5954ae1054b2fa75cd60bbaf54f614ae8e
Rahma Eliasu <eliasurahma@gmail.com>
2025-09-26 14:43:39 +0000

    Updated shopping_list_manager python file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -16,16 +16,16 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f'Added "{item}" to the shopping list.')
 
         elif choice == '2':
-            item = input("Enter the item: ")
+            item = input("Enter the item remove: ")
             shopping_list.remove(item)
             print(f'Removed "{item}" from the shoppong list.')
         elif choice == '3':
-            item = input("Enter the item: ")
+            item = input("Enter the item view: ")
             if item in shopping_list:
                 print(f'"{item}" in shopping list.')
             else:
