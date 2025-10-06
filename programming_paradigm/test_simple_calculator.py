90b5b1d396fafd4b88bd20e8947bb63ee8772785
Rahma Eliasu <eliasurahma@gmail.com>
2025-10-06 00:10:41 +0000

    update

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,10 +5,10 @@ class TestCalc(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(5, 2), 7)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self):
