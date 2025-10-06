6a704ed7098fe18906d507f56f6ec9c3922c4945
Rahma Eliasu <eliasurahma@gmail.com>
2025-10-06 00:09:15 +0000

    added new files and codes

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,19 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestCalc(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(5, 2), 7)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(5, 3), 15)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(8, 4), 2)
+        self.assertIsNone(self.calc.divide(5, 0))
